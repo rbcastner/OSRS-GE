01a6fcd9ccad27147d7da4ebe01c7b3873e7d01b
Ryan Castner <audiolion@users.noreply.github.com>
2017-06-22 22:36:54 -0400

    Style fixes (#2)

--- ge_query.py
+++ ge_query.py
@@ -6,23 +6,23 @@ def grand_exchange_query(query_type=None,itemID = None):
         'detailed' : '/api/catalogue/detail.json?item=itemID',
         'time_data': '/api/graph/itemID.json',
     }
-    
+
     query_url = type_dict[query_type]
     if 'itemID' in query_url and itemID is None:
         raise AttributeError(f'When `query_type` = {query_type}, the `itemID` must be defined')
-    
+
     if itemID is not str:
         itemID = str(itemID)
-        
+
     query_url = query_url.replace('itemID',itemID)
     full_url = base_url + query_url
-    
+
     r = requests.get(full_url)
     json_data = r.json()
-    
+
     if query_type is 'detailed':
         json_data = json_data['item']
-
     return json_data
-    
-my_data = grand_exchange_query('detailed',3002)
+
+
+my_data = grand_exchange_query('detailed', 3002)
